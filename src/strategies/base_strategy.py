8102a995d2cc31f881010a1df27783481fc233a0
LaProgrammer <justin.ebedi@uricreative.com>
2025-07-30 16:10:01 +0100

    Handle optional init arguments

--- src/strategies/base_strategy.py
+++ src/strategies/base_strategy.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Optional
 from urllib.parse import urlencode
 
 
@@ -9,15 +9,17 @@ class BaseStrategy:
         client_secret: str,
         auth_url: str,
         token_url: str,
-        user_data_url: str,
-        revoke_token_url: str,
+        user_data_url: Optional[str],
+        revoke_token_url: Optional[str],
     ):
         self.client_id = client_id
         self.client_secret = client_secret
         self.token_url = token_url
-        self.user_data_url = user_data_url
-        self.revoke_token_url = revoke_token_url
         self.auth_url = auth_url
+        if user_data_url:
+            self.user_data_url = user_data_url
+        if revoke_token_url:
+            self.revoke_token_url = revoke_token_url
 
     def generate_url(
         self,
@@ -36,3 +38,6 @@ class BaseStrategy:
 
         query_string = urlencode(parameters)
         return f"{self.auth_url}?{query_string}"
+
+    async def exchange_code_for_token(code) -> None:
+        pass
